444897d8ea9fbf7a5394fdffa7a08fcbd84d9ae5
LeopoldoEstevez <leopoldoestevezagra@gmail.com>
2023-03-20 12:29:00 +0100

    Added: Changes till leaving saturday

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,44 @@
+import { useEffect, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMagnifyingGlass } from '@fortawesome/free-solid-svg-icons';
 
 import RoundedInput from '@/components/atoms/RoundedInput';
 
 import styles from '@/styles/Home.module.css';
-import { useEffect, useState } from 'react';
 
-export default function Home() {
+type bookType = {
+  id: number,
+  created_at: string,
+  isbn: string,
+  titulo: string,
+  autor: string | null,
+  anho_publicacion: string | null,
+  editorial: string
+};
+
+const Home: React.FC= () => {
   const [inputValue, setInputValue] = useState<string>('');
+  const [books, setBooks] = useState<Array<bookType>>([]);
 
   const handleChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
     setInputValue(e.target.value);
   };
 
+  const fetchBooks = async () => {
+    const response = await fetch('/api/libros');
+    if (!response.ok) { return;}
+    
+    const json = await response.json();
+
+    setBooks(() => { return  json });
+  };
+
+  useEffect(() => {
+    fetchBooks();
+  }, [])
   return (
     <div className={styles.container}>
+      <pre> { JSON.stringify(books, null, 2)} </pre>
       <div className={styles.searchInputContainer}>
         <RoundedInput 
           icon={<FontAwesomeIcon icon={faMagnifyingGlass} />}
@@ -22,8 +46,23 @@ export default function Home() {
         />
       </div>
       <div className={styles.booklistContainer}>
+        {books && books.length && books.map((book: bookType) => {
+          return (
+            <div className={styles.card} key={book.id}>
+              <div className={styles.cardImage}></div>
+              <div className={styles.cardContent}>
+                <h3 className={styles.cardTitle}> {book.titulo} </h3>
+                <div> {book.isbn} </div>
+                <div> {book.editorial} </div>
+                <div> {new Date(book.created_at).toLocaleString()} </div>
+              </div>
 
+            </div>
+          );
+        })}
       </div>
     </div>
-  )
-}
+  );
+};
+
+export default Home;
